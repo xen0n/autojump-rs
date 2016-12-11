ec33482ea2a9250965a233c83917c66fdba140d2
Wang Xuerui <git@xen0n.name>
2016-12-12 01:43:21 +0800

    init(autojump-utils): port sanitize

---
+++ src/autojump-utils/src/input.rs
@@ -0,0 +1,53 @@
+use std::path;
+
+
+fn sanitize_one_needle(needle: &str) -> &str {
+    if needle.len() == 1 {
+        if needle.chars().next().unwrap() == path::MAIN_SEPARATOR {
+            return needle;
+        }
+    }
+
+    needle.trim_right_matches(path::MAIN_SEPARATOR)
+}
+
+
+pub fn sanitize<'a, S>(needles: &'a [S]) -> Vec<&'a str>
+        where S: AsRef<str> {
+    needles
+        .iter()
+        .map(|s| s.as_ref())
+        .map(sanitize_one_needle)
+        .collect()
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+
+    #[test]
+    fn test_sanitize_one_needle() {
+        assert_eq!(sanitize_one_needle(""), "");
+        assert_eq!(sanitize_one_needle("/"), "/");
+        assert_eq!(sanitize_one_needle("/a"), "/a");
+        assert_eq!(sanitize_one_needle("a"), "a");
+        assert_eq!(sanitize_one_needle("a/"), "a");
+        assert_eq!(sanitize_one_needle("a//"), "a");
+    }
+
+
+    #[test]
+    fn test_sanitize() {
+        let a: Vec<&str> = vec![];
+        let b: Vec<&str> = vec![];
+        assert_eq!(sanitize(&a), b);
+
+        assert_eq!(sanitize(&[""]), [""]);
+        assert_eq!(sanitize(&["foo"]), ["foo"]);
+        assert_eq!(sanitize(&["foo", "/bar"]), ["foo", "/bar"]);
+        assert_eq!(sanitize(&["foo", "/"]), ["foo", "/"]);
+        assert_eq!(sanitize(&["foo", "bar/"]), ["foo", "bar"]);
+    }
+}
--- src/autojump-utils/src/lib.rs
+++ src/autojump-utils/src/lib.rs
@@ -1,5 +1,7 @@
+mod input;
 mod shells;
 mod tabentry;
 
+pub use input::*;
 pub use shells::*;
 pub use tabentry::*;
