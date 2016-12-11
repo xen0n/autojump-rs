003ee2353e4b5973e3f6f20bdd429e1ae22fac50
Wang Xuerui <git@xen0n.name>
2016-12-11 16:01:29 +0800

    init(autojump-cli): implement current directory weight

--- src/autojump-cli/stat.rs
+++ src/autojump-cli/stat.rs
@@ -1,8 +1,17 @@
+use std::env;
+
 use autojump::Config;
 use autojump_data;
 
 
 pub fn print_stat(config: &Config) {
+    let cwd: Option<_> = match env::current_dir() {
+        Ok(dir) => Some(dir),
+        // The cwd is gone or inaccessible, disable weight reporting later.
+        Err(_) => None,
+    };
+    let mut cwd_weight: Option<f64> = None;
+
     let entries = {
         let mut tmp = autojump_data::load(config);
         tmp.sort();
@@ -14,12 +23,24 @@ pub fn print_stat(config: &Config) {
         // NOTE: This isn't exactly accurate due to floating-point nature,
         // but since this is only an estimate let's get over it!
         weight_sum += entry.weight;
+
+        // Simultaneously check for current directory's weight, if current
+        // directory is accessible.
+        if cwd.is_some() && cwd_weight.is_none() {
+            if &entry.path == cwd.as_ref().unwrap() {
+                cwd_weight = Some(entry.weight);
+            }
+        }
     }
 
     println!("________________________________________\n");
     println!("{:.0}:\t total weight", weight_sum.floor());
     println!("{}:\t number of entries", entries.len());
-    // TODO: current directory weight
+
+    if cwd.is_some() {
+        let cwd_weight = cwd_weight.unwrap_or(0.0f64);
+        println!("{:.2}:\t current directory weight", cwd_weight);
+    }
 
     println!("\ndata:\t {}", config.data_path.to_string_lossy());
 }
