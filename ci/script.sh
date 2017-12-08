f99d4d61ba152f88403d9d26637836923f4e6ecd
Wang Xuerui <git@xen0n.name>
2017-12-08 23:13:40 +0800

    chore: tweak ci configuration
    * run on PRs and develop branch for tests
    * reduce some work

--- ci/script.sh
+++ ci/script.sh
@@ -4,14 +4,12 @@ set -ex
 
 main() {
     cross build --target $TARGET
-    cross build --target $TARGET --release
 
     if [ ! -z $DISABLE_TESTS ]; then
         return
     fi
 
     cross test --target $TARGET
-    cross test --target $TARGET --release
 }
 
 # we don't run the "test phase" when doing deploys
