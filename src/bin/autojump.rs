a336019df0d16c2e8b715b4e9ddb732a5dd7af46
Wang Xuerui <git@xen0n.name>
2017-12-09 13:16:07 +0800

    chore: remove Git version info from builds

--- src/bin/autojump.rs
+++ src/bin/autojump.rs
@@ -1,14 +1,17 @@
 extern crate autojump;
 
 
-// Inspired by and taken from `rustfmt`.
-// Include git commit hash and worktree status; contents are like
-//   const COMMIT_HASH: Option<&'static str> = Some("5d53581");
-//   const WORKTREE_CLEAN: Option<bool> = Some(false);
-// with `None` if running git failed, eg if it is not installed.
-include!(concat!(env!("OUT_DIR"), "/git_info.rs"));
+/// Get a version string suitable for the CLI display.
+fn get_version_str() -> String {
+    let mut tmp = String::new();
+    tmp.push_str("autojump v");
+    tmp.push_str(autojump::VERSION_TRACK);
+    tmp.push_str("\nautojump-rs v");
+    tmp.push_str(autojump::VERSION);
 
+    tmp
+}
 
 fn main() {
-    autojump::main(autojump::get_version_str(COMMIT_HASH, WORKTREE_CLEAN));
+    autojump::main(get_version_str());
 }
--- src/lib.rs
+++ src/lib.rs
@@ -24,28 +24,3 @@ pub const VERSION_TRACK: &'static str = "22.5.0";
 /// The library's version.
 pub const VERSION: &'static str = "0.2.2";
 
-
-/// Get a version string suitable for the CLI display.
-pub fn get_version_str(vcs_commit: Option<&str>, vcs_clean: Option<bool>) -> String {
-    let mut tmp = String::new();
-    tmp.push_str("autojump v");
-    tmp.push_str(VERSION_TRACK);
-    tmp.push_str("\nautojump-rs v");
-    tmp.push_str(VERSION);
-
-    // add version control status if available
-    if let Some(commit) = vcs_commit {
-        tmp.push_str(" (");
-        tmp.push_str(commit);
-
-        if let Some(clean) = vcs_clean {
-            if !clean {
-                tmp.push_str("-dirty");
-            }
-        }
-
-        tmp.push(')');
-    }
-
-    tmp
-}
