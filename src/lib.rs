6570db19e2b0eba2024b99eae4d7553e0d7b6c7f
Wang Xuerui <git@xen0n.name>
2019-07-07 22:42:59 +0800

    chore: use 2018 edition

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,7 @@
 #![deny(warnings)]
 
-extern crate atomicwrites;
-extern crate docopt;
-extern crate regex;
-extern crate serde;
 #[macro_use]
 extern crate serde_derive;
-extern crate strsim;
-extern crate dirs;
 
 pub mod cli;
 pub mod config;
