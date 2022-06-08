e7868d969707becff7f3658f83c5dc107e7a948c
Bharatvaj <bharatvaj@yahoo.com>
2022-06-08 17:25:41 +0530

    Handle '/' in j.bat
    Fixes #125

--- integrations/j.bat
+++ integrations/j.bat
@@ -6,10 +6,10 @@ if ERRORLEVEL 1 (
   for /f "delims=" %%i in ('"%~dp0\autojump" %*') do set new_path=%%i
   if exist !new_path!\nul (
     echo !new_path!
-    pushd !new_path!
+    pushd "!new_path!"
     REM endlocal is necessary so that we can change directory for outside of this script
     REM but will automatically popd. We mush pushd twice to work around this.
-    pushd !new_path!
+    pushd "!new_path!"
     endlocal
     popd
   ) else (
